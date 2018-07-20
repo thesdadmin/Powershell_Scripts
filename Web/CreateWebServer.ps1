99251e86031e5befd33017a68239b358480bd5ef
Rob Pittman <spartacus82@outlook.com>
2018-07-20 12:17:00 -0500

    Update CreateWebServer.ps1

--- Web/CreateWebServer.ps1
+++ Web/CreateWebServer.ps1
@@ -8,4 +8,7 @@ Add-WindowsFeature Web-Server -Includeallsubfeature -verbose
 ## Enable .NET 3.5 
 Add-WindowsFeature Net-Framework-Features -includeallsubfeature -verbose 
 ## enable RDP
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" –Value 1 }
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" –Value 0
+Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\‘ -Name “UserAuthentication” -Value 1
+Enable-NetFirewallRule -DisplayGroup “Remote Desktop”
+}
