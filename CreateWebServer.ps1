e79e599a84159af428f8b48a02bbe3042c1b9763
Rob Pittman <spartacus82@outlook.com>
2018-07-20 11:36:21 -0500

    Create CreateWebServer.ps1

---
+++ CreateWebServer.ps1
@@ -0,0 +1,11 @@
+$servers="SV1","SV2","SV3"
+Invoke-Command -ComputerName $servers -ScriptBlock {
+Restart-Computer -Force -Verbose
+##Set Timezone
+Set-TimeZone -Name "Pacifice Standard Time" 
+##Install IIS
+Add-WindowsFeature Web-Server -Includeallsubfeature -verbose
+## Enable .NET 3.5 
+Add-WindowsFeature Net-Framework-Features -includeallsubfeature -verbose 
+## enable RDP
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" –Value 1 }
