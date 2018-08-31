e8974a2e9bd339c4fd3c82dca301b60bbd7ba703
Rob Pittman <spartacus82@outlook.com>
2018-08-31 16:05:40 -0500

    Create VMSCSImap.ps1

---
+++ VMware/VMSCSImap.ps1
@@ -0,0 +1,27 @@
+##Get VMware VM SCSI controller and Virtual Harddisk mapping. 
+Function Get-VMSCSIMap{
+
+Select @{N='VM';E={$_.Parent.Name}},Name,CapacityGB,
+    @{N='SCSIid';E={
+        $hd = $_
+        $ctrl = $hd.Parent.Extensiondata.Config.Hardware.Device | where{$_.Key -eq $hd.ExtensionData.ControllerKey}
+        "$($ctrl.BusNumber):$($_.ExtensionData.UnitNumber)"
+     }}}
+	 
+	 
+	 
+Get-VM TWdB02| Get-HardDisk |
+
+Select @{N='VM';E={$_.Parent.Name}},
+
+    Name,
+
+    @{N='SCSIid';E={
+
+        $hd = $_
+
+        $ctrl = $hd.Parent.Extensiondata.Config.Hardware.Device | where{$_.Key -eq $hd.ExtensionData.ControllerKey}
+
+        "$($ctrl.BusNumber):$($_.ExtensionData.UnitNumber)"
+
+     }}
