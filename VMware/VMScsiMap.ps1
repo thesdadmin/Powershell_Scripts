2170f28da121ad7ae2b875c34b5f4b053d66e91b
Rob Pittman <spartacus82@outlook.com>
2018-11-29 12:45:52 -0600

    Update VMScsiMap.ps1
    Updated the Code so that it actually works. Added Parameter bindings. Tested with local vCenter and pulled correct data.

--- VMware/VMScsiMap.ps1
+++ VMware/VMScsiMap.ps1
@@ -1,9 +1,16 @@
-﻿##Get VMware VM SCSI controller and Virtual Harddisk mapping. 
+##Get VMware VM SCSI controller and Virtual Harddisk mapping.
 Function Get-VMSCSIMap {
-
-Select @{N='VM';E={$_.Parent.Name}},Name,CapacityGB,
-    @{N='SCSIid';E={
+    param(
+    [parameter(Mandatory=$true,
+    ValueFromPipeline=$true)]
+    [String[]]
+  [array]
+  $VM
+    )
+  $vmtarget=$VM
+  $VM_harddisk=Get-Harddisk -VM $vmtarget
+  $VM_harddisk | Select @{N='VM';E={$_.Parent.Name}},Name,CapacityGB,@{N='SCSIid';E={
         $hd = $_
         $ctrl = $hd.Parent.Extensiondata.Config.Hardware.Device | where{$_.Key -eq $hd.ExtensionData.ControllerKey}
         "$($ctrl.BusNumber):$($_.ExtensionData.UnitNumber)"
-     }}
+     }}}
