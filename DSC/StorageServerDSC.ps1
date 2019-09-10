4a46f6db721071738013c140bd5cc6b8116bede6
Pittman <rpittman@morphotrust.com>
2019-09-10 16:42:04 -0500

    New files for GOVMOMI and DSC

---
+++ DSC/StorageServerDSC.ps1
@@ -0,0 +1,62 @@
+configuration StorageServer {
+##Specify node
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, iSCSIDSC
+    Node Localhost 
+    {
+        Script StoragePool {
+            SetScript = {
+            $disks=Get-StoragePool -IsPrimordial $true | Get-PhysicalDisk | Where-Object CanPool -eq $True
+            $pool=New-StoragePool –FriendlyName StoragePool1 –StorageSubsystemFriendlyName “Storage Spaces*” –PhysicalDisks $disks    
+            New-StoragePool -FriendlyName StoragePool1 -StorageSubSystemFriendlyName '*storage*' -PhysicalDisks $disks
+        }
+        TestScript = {
+            (Get-StoragePool -ErrorAction SilentlyContinue -FriendlyName StoragePool1).OperationalStatus -eq 'OK'
+        }
+        GetScript = {
+            @{Ensure = if ((Get-StoragePool -FriendlyName StoragePool1).OperationalStatus -eq 'OK') {'Present'} Else {'Absent'}}
+        }
+
+        }
+        Script StorageVolume {
+            SetScript = {
+                Get-VirtualDisk –FriendlyName VirtualDisk1 | Get-Disk | Initialize-Disk –Passthru | New-Partition –AssignDriveLetter -driveLetter E –UseMaximumSize | Format-Volume
+            }
+            TestScript = {Get-virtualdisk -friendlyname VirtualDisk1 | Get-Disk|Get-partition|Get-Volume -driveLetter E }
+            GetScript  = { @{Result =(Get-Volume -driveLetter E)}}
+
+        }
+        WindowsFeature iSCSITargetServerInstall
+            {
+                Ensure = "Present"
+                Name   = "FS-iSCSITarget-Server"
+            }
+            
+        Service ISCSITarget
+            {
+                Name   = 'WinTarget'
+                State  = 'Running'
+                Ensure = 'Present'
+                DependsOn = "[WindowsFeature]ISCSITargetServerInstall"
+
+            }
+        File ISCSIDisk
+            {
+                Type = 'Directory'
+                DestinationPath = 'E:\iSCSIVirtualDisks'
+                Ensure = "Present"
+            
+
+        iSCSIVirtualDisk iSCSIClusterVDisk01
+            {
+                Ensure      = 'Present'
+                Path        = 'E:\iSCSIVirtualDisks\Datastore.vhdx'
+                DiskType    = 'Fixed'
+                SizeBytes   = 5.9TB
+                Description = 'Datastore Virtual Disk'
+                DependsOn   = "[Service]ISCSITarget"
+            } # End of iSCSIVirtualDisk Resource
+    }
+}
+
+
+
---
+++ GOVC.ps1
@@ -0,0 +1,9 @@
+##Script to configure ENV variables for GOVC
+$GOVC_URL=Read-Host "VCenter URL"
+$GOVC_USERNAME=Read-Host "VCenter Login UN"
+$GOVC_PASSWORD= Read-Host "Vcenter Login Pass"
+$GOVC_INSECURE="TRUE"
+$env:GOVC_URL=$GOVC_URL
+$env:GOVC_USERNAME=$GOVC_USERNAME
+$env:GOVC_PASSWORD=$GOVC_PASSWORD
+$env:GOVC_INSECURE=$GOVC_INSECURE
