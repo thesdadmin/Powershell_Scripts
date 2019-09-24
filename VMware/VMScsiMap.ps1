787626d1c92e9673e3d532ef3d51e32856b86a6e
Pittman <rpittman@morphotrust.com>
2019-09-24 15:49:03 -0500

    slight grammar change

--- VMware/VMScsiMap.ps1
+++ VMware/VMScsiMap.ps1
@@ -1,6 +1,6 @@
 ##Get VMware VM SCSI controller and Virtual Harddisk mapping.
 ##Based on code from https://code.vmware.com/forums/2530/vsphere-powercli#578118
-Function Get-VMSCSIMap {
+function Get-VMSCSIMap {
     param(
     [parameter(Mandatory=$true,
     ValueFromPipeline=$true)]
@@ -10,8 +10,8 @@ Function Get-VMSCSIMap {
     )
   $vmtarget=$VM
   $VM_harddisk=Get-Harddisk -VM $vmtarget
-  $VM_harddisk | Select @{N='VM';E={$_.Parent.Name}},Name,CapacityGB,@{N='SCSIid';E={
+  $VM_harddisk | Select-Object @{N='VM';E={$_.Parent.Name}},Name,CapacityGB,@{N='SCSIid';E={
         $hd = $_
-        $ctrl = $hd.Parent.Extensiondata.Config.Hardware.Device | where{$_.Key -eq $hd.ExtensionData.ControllerKey}
+        $ctrl = $hd.Parent.Extensiondata.Config.Hardware.Device | where-object {$_.Key -eq $hd.ExtensionData.ControllerKey}
         "$($ctrl.BusNumber):$($_.ExtensionData.UnitNumber)"
      }}}
