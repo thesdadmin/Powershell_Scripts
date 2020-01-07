36d220087ecddc953dc67078c23ba193375805f2
Pittman <rpittman@morphotrust.com>
2020-01-07 15:08:46 -0600

    Updated Netstat

--- BetterNetstat.ps1
+++ BetterNetstat.ps1
@@ -1,4 +1,5 @@
 ##source https://mikefrobbins.com/2018/07/19/use-powershell-to-determine-what-your-system-is-talking-to/
+## doesn't work on systems earlier than Server 2016/Win10
 Get-NetTCPConnection -State Established |
 Select-Object -Property LocalAddress, LocalPort, RemoteAddress, RemotePort, State,
                         @{name='Process';expression={(Get-Process -Id $_.OwningProcess).Name}}, CreationTime |
